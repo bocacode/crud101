8e8d66d5b1f982ac55ac7dde3aba8ed8e706e7ee
Todd Albert <todd@bocacode.com>
2021-04-16 16:26:17 -0400

    added express

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const express = require('express')
 const admin = require('firebase-admin')
 
 const credentials = require('./credentials.json')
+
+const port = 3000
 
 admin.initializeApp({
   credential: admin.credential.cert(credentials)
@@ -8,20 +11,31 @@ admin.initializeApp({
 
 const db = admin.firestore()
 
-// NOW WE CAN CRUD!!
-db.collection('products').get()
-  .then(collection => {
-    let productsArray = []
-    collection.forEach(doc => {
-      const thisData = doc.data()
-      const thisProduct = {
-        id: doc.id,
-        name: thisData.name || thisData.Name,
-        category: thisData.category || thisData.Category,
-        vendor: thisData.vendor || thisData.Vendor,
-      }
-      productsArray.push(thisProduct)
+const app = express()
+
+app.get('/yo', (request, response) => {
+  response.send('Yo!')
+})
+
+app.get('/products', (request, response) => {
+  db.collection('products').get()
+    .then(collection => {
+      let productsArray = []
+      collection.forEach(doc => {
+        const thisData = doc.data()
+        const thisProduct = {
+          id: doc.id,
+          name: thisData.name || thisData.Name,
+          category: thisData.category || thisData.Category,
+          vendor: thisData.vendor || thisData.Vendor,
+        }
+        productsArray.push(thisProduct)
+      })
+      response.send(productsArray)
     })
-    console.log(productsArray)
-  })
-  .catch(err => console.log('Error getting products: ', err))
+    .catch(err => response.status(501).send('Error getting products: ', err))  
+})
+
+app.listen(port, () => {
+  console.log('Listening to http://localhost:' + port)
+})
